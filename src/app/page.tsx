0f39ef03dbe2d36d8ed5fc128d7a2ef28333e808
adil2190 <madil219076@gmail.com>
2024-07-16 23:58:20 +0500

    open resume setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
-import { Test } from "ui/test";
+import { Hero } from "home/Hero";
+import { Steps } from "home/Steps";
+import { Features } from "home/Features";
+import { Testimonials } from "home/Testimonials";
+import { QuestionsAndAnswers } from "home/QuestionsAndAnswers";
 
-export default function HomePage() {
+export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello World
-      <Test />
+    <main className="mx-auto max-w-screen-2xl bg-dot px-8 pb-32 text-gray-900 lg:px-12">
+      <Hero />
+      <Steps />
+      <Features />
+      <Testimonials />
+      <QuestionsAndAnswers />
     </main>
   );
 }
