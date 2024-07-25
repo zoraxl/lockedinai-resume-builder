9a1e32b68fffadb0106838465d44e5980ee57877
adil2190 <madil219076@gmail.com>
2024-07-25 17:11:35 +0500

    api issue fixed

--- src/app/lib/services/resumeService.ts
+++ src/app/lib/services/resumeService.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 import { RESUME_SERVICE_BASE_URL } from "lib/constants";
 
 export const generateFakeResumeService = (body: any) => {
-  return axios.post(`${RESUME_SERVICE_BASE_URL}/fakeairesume`, body);
+  return axios.post(`${RESUME_SERVICE_BASE_URL}/fakeairesume/`, body);
 };
